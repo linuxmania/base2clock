1be9035d8e4dd94c6d26cff1f23712bd149892e9
linuxmania <linuxmania@users.noreply.github.com>
2014-12-05 19:43:34 -0500

    Update base2clock.ino

--- base2clock.ino
+++ base2clock.ino
@@ -23,7 +23,8 @@
 //slightly fast float fudge_factor = 1.0012;
 //very slightly fast float fudge_factor = 1.0011;
 // became too fast over time float fudge_factor = 1.00107;
-float fudge_factor = 1.0006;
+// slow float fudge_factor = 1.0006;
+float fudge_factor = 1.0008;
 int offset = 15;
 
 unsigned long prev_mils,mil,sec,mins,hr,reset_mil = 0;
